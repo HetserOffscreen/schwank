c19463ccf42963ffe67a62d5e2c4d44dc5810ffd
HetserOffscreen <102925464+HetserOffscreen@users.noreply.github.com>
2026-06-28 01:08:18 -0300

    update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
-    base: '/schwank/',
+    base: '/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
